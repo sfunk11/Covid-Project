cff6bc881c0dde163cb2b5e97bd193e92a95cb00
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-02-22 18:53:31 -0500

    added basic API integration for testing sites

--- Sequelize-Passport-Example/public/js/api-integration.js
+++ Sequelize-Passport-Example/public/js/api-integration.js
@@ -7,13 +7,28 @@ $.ajax({
     console.log(data);
 });
 
-// API call to get historical cases to add to DB
+// API call to get historical info to add to DB
 function getHistory(){
-    let status = "confirmed";
+    let status = "confirmed";  //other option is "deaths"
     let country = "US";
     let queryURL = "https://covid-api.mmediagroup.fr/v1/history?";
     queryURL += "status=" + status;
     queryURL += "&country=" + country;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then((data) => {
+        console.log(data);
+    })
+}
+
+// API to call for testing locations
+function getTestingSites(lon,lat){
+    let apiKey = "f6DSSKECPwqTQkKxQSd87WsvF6m7miV1IxZ6i4ac02U";
+    let queryUrl = "https://discover.search.hereapi.com/v1/discover?";
+    queryUrl += "apikey=" + apiKey;
+    queryUrl += "&q=Covid&at=" + lon + "," + lat + "&limit=10";
 
     $.ajax({
         url: queryURL,
