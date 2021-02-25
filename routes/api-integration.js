19a290d5e6afd42204a22b06a8ea5cca58f5a4a5
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-02-24 21:08:18 -0500

    added Axios routes for other 3rd party APIs

--- routes/api-integration.js
+++ routes/api-integration.js
@@ -2,7 +2,12 @@ const axios = require("axios");
 const axiosStats = axios.create({
   baseURL: "some-https://covid-api.mmediagroup.fr"
 });
+const axiosSites = axios.create({
+  baseURL: "https://discover.search.hereapi.com"
+});
+
 module.exports = function(app) {
+  // API call for current stats
   app.get("/stats", async (req, res) => {
     try {
       const response = await axiosStats.get("/v1/cases?ab=US");
@@ -11,31 +16,36 @@ module.exports = function(app) {
       throw error;
     }
   });
+  //  API call for history information
+  app.get("/history", async (req, res) => {
+    try {
+      let country = "US";
+      let status = "confirmed";
+      let path = "/v1/history?";
+      path += "status=" + status;
+      path += "&country=" + country;
+      const response = await axiosStats.get(path);
+      console.log(response);
+    } catch (error) {
+      throw error;
+    }
+  });
+  // API call for testing locations
+  app.get("/sites/:lon/:lat", async (req, res) => {
+    try {
+      let lon = req.params.lon;
+      let lat = req.params.lat;
+      const apiKey = "f6DSSKECPwqTQkKxQSd87WsvF6m7miV1IxZ6i4ac02U";
+      let path = "v1/discover?";
+      path += "apikey=" + apiKey;
+      path += "&q=Covid&at=" + lon + "," + lat + "&limit=10";
+      const response = await axiosSites.get(path);
+      console.log(response);
+    } catch (error) {
+      throw error;
+    }
+  });
 };
-// $.ajax({
-//   url: "https://covid-api.mmediagroup.fr/v1/cases?ab=US",
-//   method: "GET"
-// }).then(data => {
-//   console.log(data);
-//   let stats = JSON.parse(data);
-//   console.log(stats);
-// });
-
-// // API call to get historical info to add to DB
-// function getHistory() {
-//   const status = "confirmed"; //other option is "deaths"
-//   const country = "US";
-//   let queryURL = "https://covid-api.mmediagroup.fr/v1/history?";
-//   queryURL += "status=" + status;
-//   queryURL += "&country=" + country;
-
-//   $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(data => {
-//     console.log(data);
-//   });
-// }
 
 // // API to call for testing locations
 // function getTestingSites(lon, lat) {
